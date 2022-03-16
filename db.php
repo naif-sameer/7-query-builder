63668b1888eb8300a0df99e602a77101dfca586e
Naif <nayef7138@gmail.com>
2022-03-16 15:54:43 +0300

    update db.php

--- db.php
+++ db.php
@@ -1,9 +1,5 @@
 <?php
 
-namespace DB;
-
-use PDO;
-
 $dns = "mysql:host=localhost";
 $username = 'root';
 $password = '';
@@ -13,7 +9,7 @@ $database_name = 'e-shop';
 /**
  * where
  */
-trait Where
+trait where
 {
   public function where(string $key, string $value)
   {
@@ -21,12 +17,114 @@ trait Where
 
     return $this;;
   }
+
+  public function orWhere(string $key, string $value)
+  {
+    $this->db_query .= " OR `$key` = '$value'";
+
+    return $this;;
+  }
 }
+
+/**
+ * runQuery functions
+ */
+trait runQuery
+{
+  public function get()
+  {
+    $statement = $this->conn->prepare($this->db_query);
+    $statement->execute();
+
+    return $statement->fetch();
+  }
+
+  public function getAll()
+  {
+    $statement = $this->conn->prepare($this->db_query);
+    $statement->execute();
+
+    return $statement->fetchAll();
+  }
+
+  public function run()
+  {
+    return $this->conn->prepare($this->db_query)->execute();
+  }
+}
+
+/**
+ * join
+ */
+trait join
+{
+
+  public function innerJoin(string $table_name, array $table_list)
+  {
+    if (count($table_list) !== 2) {
+      throw new Exception('table list must be two items');
+    }
+
+    $select_tables = "'{$table_list[0]}' = '{$table_list[1]}'";
+
+    $this->db_query .= " INNER JOIN `{$table_name}` ON " . $select_tables;
+    return $this;
+  }
+
+  public function leftJoin(string $table_name, array $table_list)
+  {
+    if (count($table_list) !== 2) {
+      throw new Exception('table list must be two items');
+    }
+
+    $select_tables = "'{$table_list[0]}' = '{$table_list[1]}'";
+
+    $this->db_query .= " LEFT JOIN `{$table_name}` ON " . $select_tables;
+    return $this;
+  }
+
+  public function outerJoin(string $table_name, array $table_list)
+  {
+    if (count($table_list) !== 2) {
+      throw new Exception('table list must be two items');
+    }
+
+    $select_tables = "'{$table_list[0]}' = '{$table_list[1]}'";
+
+    $this->db_query .= " FULL OUTER JOIN `{$table_name}` ON " . $select_tables;
+    return $this;
+  }
+}
+
+/**
+ * queryBy
+ */
+trait queryBy
+{
+  public function orderBy(string $column_name, string $order_type)
+  {
+    $this->db_query .= " ORDER BY $column_name $order_type ";
+
+    return $this;
+  }
+
+  public function groupBy(string $column_name)
+  {
+    $this->db_query .= " GROUP BY $column_name  ";
+
+    return $this;
+  }
+}
+
+
+
+
+
 
 
 class Database
 {
-  use Where;
+  use where, runQuery, join, queryBy;
 
   private $conn;
   private $table_name;
@@ -35,7 +133,6 @@ class Database
   public function __construct(string $table_name, array $table_data)
   {
     global $dns, $username, $password, $database_name;
-    $this->table_name = $table_name;
 
     $this->conn = new PDO($dns, $username, $password, [
       PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -63,36 +160,24 @@ class Database
   }
 
   public function select(
+    string $table_name,
     array $table_columns = ["*"]
   ) {
+    $columns_name = "";
+    foreach ($table_columns as $column) {
+      $columns_name .= "'$column' ,";
+    }
+    // remove last comma
+    $columns_name = substr($columns_name, 0, -1);
 
-    $columns_name = implode(',', $table_columns);
-    $query = "SELECT $columns_name FROM {$this->table_name} ";
+    $query = "SELECT $columns_name FROM `{$table_name}` ";
 
     $this->db_query = $query;
 
     return $this;
   }
 
-
-
-  public function get()
-  {
-    $statement = $this->conn->prepare($this->db_query);
-    $statement->execute();
-
-    return $statement->fetch();
-  }
-
-  public function getAll()
-  {
-    $statement = $this->conn->prepare($this->db_query);
-    $statement->execute();
-
-    return $statement->fetchAll();
-  }
-
-  public function insert(array $table_data)
+  public function insert(string $table_name, array $table_data)
   {
     $columns = '';
     $values = '';
@@ -105,12 +190,12 @@ class Database
     $columns = substr($columns, 0, -1);
     $values = substr($values, 0, -1);
 
-    $query = "INSERT INTO `{$this->table_name}` ($columns) VALUES ({$values})";
+    $query = "INSERT INTO `{$table_name}` ($columns) VALUES ({$values})";
 
     return $this->conn->prepare($query)->execute();
   }
 
-  public function update(int $id, array $table_data)
+  public function update(string $table_name, array $table_data)
   {
     $params = [];
 
@@ -118,17 +203,66 @@ class Database
       $params[] = "`$key` = '$value'";
     }
 
-    $query  = "UPDATE {$this->table_name} SET " . implode(',', $params) .  " WHERE `id` = $id";
+    $this->db_query  = "UPDATE `{$table_name}` SET " . implode(',', $params);
 
-    $this->conn->prepare($query)->execute();
+    return $this;
   }
 
-  public function delete(int $id)
+  public function delete(string $table_name)
   {
-    $query = "DELETE FROM `{$this->table_name}` WHERE `id` = $id";
+    $this->db_query = "DELETE FROM `{$table_name}` ";
 
-    $this->conn->prepare($query)->execute();
+    return $this;
+  }
+
+  public function count(string $table_name, string $table_column)
+  {
+    $this->db_query = " SELECT COUNT('$table_column') FROM `$table_name` ";
+
+    return $this;
+  }
+
+  public function limit(int $number)
+  {
+    $this->db_query .= " LIMIT $number ";
+
+    return $this;
   }
 }
 
 $user = new Database('user', ["name" => "int"]);
+
+// print_r(
+//   $user
+//     ->select('users', ["name" => "naif"])
+//     ->where('id', '1')
+//     ->orWhere('name', 'ali')
+// );
+
+// print_r(
+//   $user
+//     ->select('users', ['users.id', 'posts.id'])
+//     ->innerJoin("customers", ["users.id", "customers.id"])
+// );
+
+// print_r(
+//   $user
+//     ->select('users', ['users.id', 'posts.id'])
+//     ->orderBy("customers", "DESC")
+// );
+
+// print_r(
+//   $user
+//     ->count('users', 'id')
+//     ->orderBy("id", "DESC")
+// );
+
+print_r(
+  $user
+    ->select('users', ['id'])
+    ->limit(1)
+);
+
+
+// you need to use run() if you want to run the query
+// use get or getAll if you want to get data;
